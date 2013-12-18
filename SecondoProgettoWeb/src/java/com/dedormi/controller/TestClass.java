60b6cf5700ba5f24347df518da737f6792bc79ed
Davide <Davide@10.22.74.193>
2013-12-18 18:02:56 +0100

    init

---
+++ SecondoProgettoWeb/src/java/com/dedormi/controller/TestClass.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.dedormi.controller;
+
+/**
+ *
+ * @author Davide
+ */
+public class TestClass {
+    
+}
