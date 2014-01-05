538cff7927450cf216267e107ff056aaea74c426
Francesco Serra <afnecors@gmail.com>
2014-01-05 15:17:24 +0100

    LogoutServlet

--- SecondoProgettoWeb/src/java/com/deadormi/servlet/EditProfileServlet.java
+++ SecondoProgettoWeb/src/java/com/deadormi/servlet/EditProfileServlet.java
@@ -83,22 +83,7 @@ public class EditProfileServlet extends HttpServlet {
         String edit = request.getParameter("whatEdit");
 
         if ("editAvatar".equals(edit)) {
-            response.setContentType("text/html;charset=UTF-8");
-            PrintWriter out = response.getWriter();
-            try {
-                /* TODO output your page here. You may use following sample code. */
-                out.println("<!DOCTYPE html>");
-                out.println("<html>");
-                out.println("<head>");
-                out.println("<title>Servlet NewServlet</title>");
-                out.println("</head>");
-                out.println("<body>");
-                out.println("<h1>CAMBIA AVATAR</h1>");
-                out.println("</body>");
-                out.println("</html>");
-            } finally {
-                out.close();
-            }
+        
         }
 
         if ("editName".equals(edit)) {
--- SecondoProgettoWeb/web/secure/home.jsp
+++ SecondoProgettoWeb/web/secure/home.jsp
@@ -2,6 +2,8 @@
 <%@page contentType="text/html" pageEncoding="UTF-8"%>
 
 <%@include file="/layout/head.jsp" %>
+<a href="/SecondoProgettoWeb/LogoutServlet">logout</a>
+
 <h1>Benvenuto, ${user.username} ! </h1><br />
 psw: ${user.password}<br />
 email:${user.email}<br />
@@ -10,15 +12,13 @@ avatar:${user.avatar_name}<br />
 id:${user.id}<br />
 login_date:${user.login_date}
 
+<br />
+<br />
 <a href="/SecondoProgettoWeb/HomeServlet?next=edit">Edita il mio profilo</a><br />
 <a href="/SecondoProgettoWeb/HomeServlet?next=groups">Gruppi</a><br />
 <a href="/SecondoProgettoWeb/secure/InviteServlet">Inviti</a><br />
 <c:if test="${user.moderator}">
 <a href="/SecondoProgettoWeb/secure/ModeratorServlet">ModeratorPage</a>
 </c:if>
-
-
-
-
 
 <%@include file="/layout/foot.jsp" %>
