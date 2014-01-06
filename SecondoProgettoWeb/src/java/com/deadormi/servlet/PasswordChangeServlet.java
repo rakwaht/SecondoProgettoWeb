fde28f9ea05ed47899c728c34c271cda1b06a0fc
Francesco Serra <afnecors@gmail.com>
2014-01-06 17:04:45 +0100

    RecoveryPasswordServlet finished

--- SecondoProgettoWeb/src/java/com/deadormi/servlet/PasswordChangeServlet.java
+++ SecondoProgettoWeb/src/java/com/deadormi/servlet/PasswordChangeServlet.java
@@ -32,36 +32,6 @@ public class PasswordChangeServlet extends HttpServlet {
 
     static Logger log = Logger.getLogger(PasswordChangeServlet.class);
 
-    /**
-     * Processes requests for both HTTP
-     * <code>GET</code> and
-     * <code>POST</code> methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        PrintWriter out = response.getWriter();
-        try {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet PasswordChangeservlet</title>");
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet PasswordChangeservlet at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
-        } finally {
-            out.close();
-        }
-    }
-
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
     /**
      * Handles the HTTP
@@ -75,7 +45,7 @@ public class PasswordChangeServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        response.sendRedirect("password_recovery.jsp");
     }
 
     /**
@@ -100,60 +70,62 @@ public class PasswordChangeServlet extends HttpServlet {
         Password_Change pc = null;
         String message;
 
+        // se l'hash è vuoto o null
         if (psw_change_id.equals("") || psw_change_id == null) {
-
-            log.debug("------------------------------------");
             message = "La sessione è scaduta. Inserisci di nuovo la email per inviare una nuova richiesta di cambio password.";
             response.sendRedirect("password_recovery.jsp?message_password=" + URLEncoder.encode(message, "UTF-8"));
         } else {
-
-            log.debug("eccccckckckckckckckckckck");
-
             try {
                 pc = pcc.getPassword_ChangeById(psw_change_id);
-                db_date = pc.getPassword_date();
             } catch (SQLException ex) {
                 log.warn(ex);
             }
 
-            Date date_now = null;
-            Date date_db = null;
-            try {
-                date_now = CurrentDate.toDate(CurrentDate.getCurrentDate());
-                date_db = CurrentDate.toDate(db_date);
-            } catch (ParseException ex) {
-                java.util.logging.Logger.getLogger(PasswordChangeServlet.class.getName()).log(Level.SEVERE, null, ex);
-            }
-
-
-            log.debug("adesso: " + date_now);
-            log.debug("dbdb: " + date_db);
-
-            long diff = (date_now.getTime() - date_db.getTime()) / 1000;
-            log.debug("diff: " + diff);
-
-            // Se passano più di 180 sec la sessione scade e bye bye
-            if (diff > 180) {
+            // se non trova l'hash nel db messaggio di errore
+            if (pc == null) {
                 message = "La sessione è scaduta. Inserisci di nuovo la email per inviare una nuova richiesta di cambio password.";
                 response.sendRedirect("password_recovery.jsp?message_password=" + URLEncoder.encode(message, "UTF-8"));
             } else {
-                UserController uc = new UserController();
-                User u = pc.getUser();
+                db_date = pc.getPassword_date();
 
-                if (password.equals(password_confirm) && !password.trim().equals("") && password.length() >= 5) {
-                    try {
-                        u = uc.updatePassword(u, Md5.getMD5(password));
-                        message = "Password cambiata con successo";
-                        response.sendRedirect("password_change.jsp?message_password=" + URLEncoder.encode(message, "UTF-8"));
-                    } catch (SQLException ex) {
-                        log.warn(ex);
-                    }
-                } else {
-                    message = "Nuova password non valida. Le password devono coincidere ed essere di lunghezza minima 5 caratteri.";
-                    response.sendRedirect("password_change.jsp?psw_change_id=" + psw_change_id + "&message_password=" + URLEncoder.encode(message, "UTF-8"));
+                Date date_now = null;
+                Date date_db = null;
+                try {
+                    date_now = CurrentDate.toDate(CurrentDate.getCurrentDate());
+                    date_db = CurrentDate.toDate(db_date);
+                } catch (ParseException ex) {
+                    log.warn(ex);
                 }
-            }
 
+                log.debug("adesso: " + date_now);
+                log.debug("dbdb: " + date_db);
+
+                long diff = (date_now.getTime() - date_db.getTime()) / 1000;
+                log.debug("diff: " + diff);
+
+                // Se passano più di 180 sec la sessione scade e bye bye
+                if (diff > 180) {
+                    message = "La sessione è scaduta. Inserisci di nuovo la email per inviare una nuova richiesta di cambio password.";
+                    response.sendRedirect("password_recovery.jsp?message_password=" + URLEncoder.encode(message, "UTF-8"));
+                } else {
+                    UserController uc = new UserController();
+                    User u = pc.getUser();
+
+                    if (password.equals(password_confirm) && !password.trim().equals("") && password.length() >= 5) {
+                        try {
+                            u = uc.updatePassword(u, Md5.getMD5(password));
+                            message = "Password cambiata con successo";
+                            response.sendRedirect("password_change.jsp?message_password=" + URLEncoder.encode(message, "UTF-8"));
+                        } catch (SQLException ex) {
+                            log.warn(ex);
+                        }
+                    } else {
+                        message = "Nuova password non valida. Le password devono coincidere ed essere di lunghezza minima 5 caratteri.";
+                        response.sendRedirect("password_change.jsp?psw_change_id=" + psw_change_id + "&message_password=" + URLEncoder.encode(message, "UTF-8"));
+                    }
+                }
+
+            }
         }
     }
 
--- SecondoProgettoWeb/web/password_change.jsp
+++ SecondoProgettoWeb/web/password_change.jsp
@@ -7,9 +7,7 @@
 <%@page contentType="text/html" pageEncoding="UTF-8"%>
 <%@include file="/layout/head.jsp" %>
 
-<h1>Ciao imposta la tua nuova password!</h1>
-<h2>WORK IN PROG!</h2>
-<h3>id richiesta: ${param.psw_change_id}</h3>
+<h1>Cambia password</h1>
 
 <form method="post" action="PasswordChangeServlet?psw_change_id=${param.psw_change_id}">
 Nuova password: <input type='password' name='password' /><br />
