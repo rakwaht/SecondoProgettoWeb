27251196cfeaf9a11988f49dae15ba0c8c807ebe
Francesco Serra <afnecors@gmail.com>
2014-01-07 12:54:21 +0100

    working on AcceptInviteServlet 2

--- SecondoProgettoWeb/src/java/com/deadormi/servlet/AcceptInviteServlet.java
+++ SecondoProgettoWeb/src/java/com/deadormi/servlet/AcceptInviteServlet.java
@@ -11,6 +11,7 @@ import com.deadormi.controller.UserController;
 import com.deadormi.entity.Crew;
 import com.deadormi.entity.Crew_User;
 import com.deadormi.entity.User;
+import com.deadormi.util.Parser;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.URLEncoder;
@@ -48,47 +49,72 @@ public class AcceptInviteServlet extends HttpServlet {
 
         UserController uc = new UserController();
 
+        String message = null;
         String email_receiver = request.getParameter("e_r");
-        Integer id_crew = Integer.parseInt(request.getParameter("id_c"));
+        String id_crew_s = request.getParameter("id_c");
 
-        User receiver = null;
+        log.debug("id_crew: " + id_crew_s);
+        log.debug("email_receiver: " + email_receiver);
+        
+        // se l'id della crew non è null ed è un numero
+        if (id_crew_s != null && Parser.isNumeric(id_crew_s)) {
 
-        try {
-            receiver = uc.findUserByEmail(email_receiver);
-        } catch (SQLException ex) {
-            log.warn(ex);
+            Integer id_crew = Integer.parseInt(id_crew_s);
+            User receiver = null;
+
+            try {
+                receiver = uc.findUserByEmail(email_receiver);
+            } catch (SQLException ex) {
+                log.warn(ex);
+            }
+
+            // se trova l'user receiver
+            if (receiver != null) {
+                CrewController crew_c = new CrewController();
+                Crew crew = null;
+                try {
+                    crew = crew_c.find_crew_by_id(id_crew);
+                } catch (SQLException ex) {
+                    log.warn(ex);
+                }
+
+                // se trova il gruppo
+                if (crew != null) {
+                    InviteController ic = new InviteController();
+                    Crew_UserController cuc = new Crew_UserController();
+                    Crew_User cu = new Crew_User();
+
+                    cu.setId_crew(id_crew);
+                    cu.setId_user(receiver.getId());
+                    cu.setCrew_user_enabled(Boolean.TRUE);
+
+                    try {
+                        ic.disableInvite(receiver.getId(), id_crew);
+                        cuc.create_crew_user(cu);
+                    } catch (SQLException ex) {
+                        log.warn(ex);
+                    }
+
+                    message = "accepted";
+                    RequestDispatcher rd = request.getRequestDispatcher("email_accept_invite.jsp?message=" + URLEncoder.encode(message, "UTF-8"));
+                    request.setAttribute("crew", crew);
+                    rd.forward(request, response);
+                } else {
+                    // crew non valida
+                    message = "not_valid";
+                    response.sendRedirect("email_accept_invite.jsp?message=" + URLEncoder.encode(message, "UTF-8"));
+                }
+            } else {
+                // user non valido
+                message = "not_valid";
+                response.sendRedirect("email_accept_invite.jsp?message=" + URLEncoder.encode(message, "UTF-8"));
+            }
+
+        } else {
+            // id crew non è un numero
+            message = "not_valid";
+            response.sendRedirect("email_accept_invite.jsp?message=" + URLEncoder.encode(message, "UTF-8"));
         }
-
-        InviteController ic = new InviteController();
-        Crew_UserController cuc = new Crew_UserController();
-        Crew_User cu = new Crew_User();
-        
-        cu.setId_crew(id_crew);
-        cu.setId_user(receiver.getId());
-        cu.setCrew_user_enabled(Boolean.TRUE);
-
-        try {
-            ic.disableInvite(receiver.getId(), id_crew);
-            cuc.create_crew_user(cu);
-        } catch (SQLException ex) {
-            log.warn(ex);
-        }
-        
-        CrewController crew_c = new CrewController();
-        Crew crew = null;
-        try {
-            crew = crew_c.find_crew_by_id(id_crew);
-        } catch (SQLException ex) {
-            log.warn(ex);
-        }
-        
-        String message = "accepted";
-        //response.sendRedirect("email_accept_invite.jsp?message=" + URLEncoder.encode(message, "UTF-8"));
-
-        RequestDispatcher rd = request.getRequestDispatcher("email_accept_invite.jsp?message=" + URLEncoder.encode(message, "UTF-8"));
-        request.setAttribute("crew", crew);
-        rd.forward(request, response);
-        
     }
 
     /**
