e40b36ebe093450024de4cd1b4b0c4916aa74760
Davide <Davide@10.22.74.193>
2014-01-12 16:46:47 +0100

    mail parser 2.0

--- SecondoProgettoWeb/src/java/com/deadormi/util/Parser.java
+++ SecondoProgettoWeb/src/java/com/deadormi/util/Parser.java
@@ -111,12 +111,11 @@ public class Parser {
     
     public static ArrayList<Post> censura_mail(ArrayList<Post> posts) {
         log.debug("Sono nel parser!");
-        Pattern p = Pattern.compile(EMAIL_PATTERN);
+        Pattern p = Pattern.compile("(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])");
         for(int i=0; i<posts.size(); i++){
             log.debug("Analizzo il post # " + i);
             String text = posts.get(i).getText();
             Matcher m = p.matcher(text);
-            log.debug(m.find());
             while(m.find()){
                 String email = text.substring(m.start(), m.end());
                 log.debug("Ho trovato una mail: " + email);
